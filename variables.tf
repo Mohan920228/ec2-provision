4cc6c7665cafc653e729dd4117fceed1bb716545
Mohan920228 <100313825+Mohan920228@users.noreply.github.com>
2025-12-30 12:29:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,12 @@
-variable "ami" {}
-variable "instance_type" {}
-variable "subnet_id" {}
-variable "vpc_security_group_ids" {}
+variable "ami" {
+default = "ami-068c0051b15cdb816"
+}
+variable "instance_type" {
+default = "t3a.nano"
+}
+variable "subnet_id" {
+default = "subnet-0a54e06d228e37fc2"
+}
+variable "vpc_security_group_ids" {
+default = [ "sg-0255e9a92d037cd32" ]
+}
