9fcb3a559a28a9a7c5de845a44a31c7a808239f1
mohan92 <mohan920228@gmail.com>
2025-12-30 04:45:34 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "aws_instance" "ec2-instance" {
+  ami = "ami-068c0051b15cdb816"
+  instance_type = "t3a.nano"
+  subnet_id = "subnet-0a54e06d228e37fc2"
+  vpc_security_group_ids = [ "sg-0255e9a92d037cd32" ]
+}
---
+++ variables.tfvars
@@ -0,0 +1,4 @@
+ami = "ami-068c0051b15cdb816"
+instance_type = "t3a.nano"
+subnet_id = "subnet-0a54e06d228e37fc2"
+vpc_security_group_ids = [ "sg-0255e9a92d037cd32" ]
